a0abcc2d2289ff267ae97542b3d9c963c0b44bc8
aldodwrzy <aldodwrzy@gmail.com>
2022-01-10 08:29:27 +0700

    Ngebenerin View/Auth/Login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,10 +2,8 @@
     <div class="card ">
         <div class="card-header text-center fa-5x"><i class="fas fa-user-circle"></i><span class="splash-description">Selamat Datang, Silahkan Login!</span>
         </div>
-        <div class="alert alert-warning alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <?= $this->session->flashdata('message'); ?>
-        </div>
+
+        <?= $this->session->flashdata('message'); ?>
         <div class="card-body">
             <form class="user" method="post" action="<?= base_url('auth'); ?>">
                 <div class="form-group">
