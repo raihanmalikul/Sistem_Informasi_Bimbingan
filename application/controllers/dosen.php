ffd8c6f91023b3402701c43cb201c171af9da3ee
raihanmalikul <raihanmalikulmulki@gmail.com>
2022-01-20 03:37:26 +0700

    bismilah minggu ini selesai

--- application/controllers/dosen.php
+++ application/controllers/dosen.php
@@ -3,11 +3,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class dosen extends CI_Controller
 {
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     is_logged_in();
-    // }
+    public function __construct()
+    {
+
+        parent::__construct();
+        if (!$this->session->userdata('user_id')) {
+            redirect('auth');
+        }
+    }
     public function index()
     {
         $data['title'] = 'Menu Dosen';
