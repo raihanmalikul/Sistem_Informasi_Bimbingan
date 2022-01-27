fc1d5c96967eec8856a865e4e14e270c582269ca
aldodwrzy <aldodwrzy@gmail.com>
2022-01-27 10:40:26 +0700

    Interface Surat Izin Sidang - Mahasiswa

--- application/views/mahasiswa/surat_izin_sidang.php
+++ application/views/mahasiswa/surat_izin_sidang.php
@@ -1,7 +1,7 @@
  <!-- wrapper  -->
  <!-- ============================================================== -->
- <div class="dashboard-wrapper">
-     <div class="container-fluid dashboard-content">
+<div class="dashboard-wrapper">
+    <div class="container-fluid dashboard-content">
          <!-- ============================================================== -->
          <!-- pageheader -->
          <!-- ============================================================== -->
@@ -16,142 +16,129 @@
          <!-- ============================================================== -->
          <!-- end pageheader -->
          <!-- ============================================================== -->
-    <!-- <div class="container d-flex justify-content-center mt-50 mb-50">
-        <div class="row">
-            <div class="col-md-12 mt-5  mb-3">
-                <button class="btn btn-primary" id="download"> download pdf</button>
-            </div>
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="container" style="padding: 15px;" id="invoice">
-                        <div class="header" style="text-align: center;">
-                            <h2><u>SURAT PERSETUJUAN DOSEN WALI</u></h2>
-                        </div>
-                        <div class="name">
-                            <p>Dengan ini saya :</p>
-                            <table style="margin-left: -5px;margin-top: -15px;">
-                                <tr>
-                                    <td>Nama</td>
-                                    <td>:</td>
-                                    <td>Muhammad Ilham</td>
-                                </tr>
-                                <tr>
-                                    <td>NIP</td>
-                                    <td>:</td>
-                                    <td>1.19.30.19</td>
-                                </tr>
-                                <tr>
-                                    <td>Nama Perguruan Tinggi</td>
-                                    <td>:</td>
-                                    <td>Politeknik Pos Indonesia</td>
-                                </tr>
-                            </table>
-                            <br>
-                            <p>menerangkan bahwa saya adalah Dosen Wali dari :</p>
-                            <table style="margin-left: -5px; margin-top: -15px;">
-                                <tr>
-                                    <td>Nama Mahasiswa</td>
-                                    <td>:</td>
-                                    <td>Muhammad Raihan Nur Azmii</td>
-                                </tr>
-                                <tr>
-                                    <td>NIM/NPM</td>
-                                    <td>:</td>
-                                    <td>1.19.30.20</td>
-                                </tr>
-                                <tr>
-                                    <td>Nama Perguruan Tinggi</td>
-                                    <td>:</td>
-                                    <td>Politeknik Pos Indonesia</td>
-                                </tr>
-                            </table>
-                            <br>
-                            <p>sekaligus memberikan izin kepada yang bersangkutan untuk mengambil MK melalui Program
-                                Pertukaran Mahasiswa dengan daftar sebagai berikut :</p>
-                        </div>
-                        <div class="tabel-mk">
-                            <table>
-                                <thead>
-                                    <th>No</th>
-                                    <th>Nama MK</th>
-                                    <th>Jumlah SKS*</th>
-                                    <th>Nama Program Studi</th>
-                                    <th>Nama Perguruan Tinggi</th>
-                                    <th>Izinkan?**</th>
-                                </thead>
-                                <tbody>
+            
+         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="card">
+                <div class="card-body">
+                     <button type="button" class="btn btn-primary text-white" id="download"><i class="fas fa-download"></i> Download PDF</button>
+                     <!-- <h4 class="card-title">Surat Persetujuan dan Permohonan Sidang</h4> -->
+                     <br>
+                     <hr>
+ 
+               
+                    <div class="row justify-content-center mt-50 mb-50">
+                        <div class="col-md-9">
+                            <div class="border">
+                                <div class="container" style="padding: 15px;" id="invoice">
+                                    <div class="header" style="text-align: center;">
+                                    <br>
+                                        <h2><u>PERSETUJUAN DAN PERMOHONAN SIDANG PROYEK</u></h2>
+                                    </div>
+                                    <br>
+                                    <div class="name">
+                                        <p>Saya sebagai Pembimbing mahasiswa tersebut dibawah ini:</p>
+                                        <table style="margin-left: 10px;margin-top: -15px;">
+                                            <tr>
+                                                <td>Nama Mahasiswa 1/Mahasiswa 2</td>
+                                                <td>:</td>
+                                                <td>...</td>
+                                                <td>/</td>
+                                                <td>...</td>
+                                            </tr>
+                                            <tr>
+                                                <td>NPM 1/ NPM 2</td>
+                                                <td>:</td>
+                                                <td>...</td>
+                                                <td>/</td>
+                                                <td>...</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Judul Proyek</td>
+                                                <td>:</td>
+                                                <td>...</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Nama Pembimbing</td>
+                                                <td>:</td>
+                                                <td>...</td>
+                                            </tr>
+                                        </table>
+                                        <br>
+                                        <p>Menyatakan bahwa mahasiswa tersebut telah menyelesaikan Alat/Proyek 100%. Adapun penulisan laporan proyek telah diselesaikan seluruhnya 100%</p>
+                                        <p>Dengan demikian saya mengajukan mahasiswa tersebut untuk mengikuti sidang Proyek. Apabila ternyata pernyataan saya tersebut tidak benar, maka saya menyetujui penundaan sidang termasuk pembatalan sidang Proyek untuk mahasiswa bimbingan saya tersebut sesuai aturan yang berlaku.</p>
+                                    </div>
+                                        <br>
+                                        <br>
+                                    <div class="tabel-mk signature-container">
+                                        <span class="signature">Bandung, ..... 2022</span><br>
+                                        <br>
+                                        <br>
+                                    </div>
+                                    <br>
+                                    <br>
+                                    <div class="row">
+                                    <table style="margin-left: 20px;margin-top: -15px;">
+                                    <!-- ------------------------------------------------------ -->
+                                    <!-- <tr>
+                                        <td>Mahasiswa 1</td>
+                                    </tr> -->
+                                    </table>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <table>
                                     <tr>
-                                        <td>1</td>
-                                        <td>Sistem Tersebar</td>
-                                        <td>3</td>
-                                        <td>Teknik Informatika</td>
-                                        <td>Politeknik Pos Indonesia</td>
-                                        <td>Ya/Tidak</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Sistem Tersebar</td>
-                                        <td>3</td>
-                                        <td>Teknik Informatika</td>
-                                        <td>Politeknik Pos Indonesia</td>
-                                        <td>Ya/Tidak</td>
+                                        <td>(................................................)</td>
                                     </tr>
                                     <tr>
-                                        <td>3</td>
-                                        <td>Sistem Tersebar</td>
-                                        <td>3</td>
-                                        <td>Teknik Informatika</td>
-                                        <td>Politeknik Pos Indonesia</td>
-                                        <td>Ya/Tidak</td>
+                                        <td>NPM ......................................</td>
                                     </tr>
-                                    <tr class="footer-table">
-                                        <th colspan="2">Total Jumlah SKS</th>
-                                        <th>9</th>
-                                        <th colspan="3"></th>
+                                    </table>
+                                    <!-- ------------------------------------------------------ -->
+                                    <table style="margin-left: 80px;">
+                                    <!-- <tr>
+                                        <td>Mahasiswa 2</td>
+                                    </tr> -->
+                                    </table>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <table>
+                                    <tr>
+                                        <td>(................................................)</td>
                                     </tr>
-                                </tbody>
-                            </table>
-                            <span>* : Tidak boleh melebihi 12 SKS</span>
-                            <br>
-                            <span>** : Coret pada bagian tidak perlu</span>
-                            <p>daftar tersebut akan menambah daftar MK Aplikasi Pertukaran Mahasiswa yang sebelumnya
-                                telah saya setujui pada semester yang sama, yaitu :</p>
-                            <div class="tabel-sks">
-                                <table>
-                                    <thead>
-                                        <th>No</th>
-                                        <th>Nama MK***</th>
-                                        <th>Jumlah SKS*</th>
-                                        <th>Nama Program Studi</th>
-                                        <th>Nama Perguruan Tinggi</th>
-                                        <th>Status</th>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="footer-table">
-                                            <th colspan="2">Total Jumlah SKS</th>
-                                            <th>0</th>
-                                            <th colspan="3"></th>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                                <span>*** : Isi jika sebelumnya telah mengambil MK pada semester yang sama</span>
+                                    <tr>
+                                        <td>NPM ......................................</td>
+                                    </tr>
+                                    </table>
+                                    <!-- ------------------------------------------------------ -->
+                                    <table style="margin-left: 90px;">
+                                    <!-- <tr>
+                                        <td>Dosen Pembimbing</td>
+                                    </tr> -->
+                                    </table>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <table>
+                                    <tr>
+                                        <td>(................................................)</td>
+                                    </tr>
+                                    <tr>
+                                        <td>NIK .........................................</td>
+                                    </tr>
+                                    </table>
+                                    </row>
+                                </div>
                             </div>
-                            <p>Demikian surat persetujuan ini dibuat untuk digunakan sebagaimana mestinya.</p>
-                        </div>
-                        <div class="signature-container">
-                            <span class="signature">..................., 14 Januari 2022</span><br>
-                            <span>.............................................</span><br>
-                            <span>NIP: .....................................</span>
-                        </div>
-                        <div class="signature2-container">
-                            <span>.............................................</span><br>
-                            <span>NIP: .....................................</span>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+            
+                 </div>
+             </div>
+         </div>
     </div>
+</div>
     <script>
         window.onload = function() {
             document.getElementById("download")
@@ -163,7 +150,7 @@
                         margin: 1,
                         filename: 'myfile.pdf',
                         image: {
-                            type: 'jpeg',
+                            type: 'pdf',
                             quality: 0.98
                         },
                         html2canvas: {
@@ -178,4 +165,4 @@
                     html2pdf().from(invoice).set(opt).save();
                 })
         }
-    </script> -->
+    </script>
