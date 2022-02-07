fb324c202725a0bb6518be3c22470affe6b5c6e1
aldodwrzy <aldodwrzy@gmail.com>
2022-02-08 06:34:09 +0700

    File template laporan

--- application/views/mahasiswa/templet_laporan_proyek.php
+++ application/views/mahasiswa/templet_laporan_proyek.php
@@ -3,7 +3,7 @@
  <div class="dashboard-wrapper">
      <div class="container-fluid dashboard-content">
          <center>
-             <iframe type="application/pdf" src="<?= base_url(); ?>/File/Template Laporan Proyek.pdf" width="1000" height="1200"></iframe>
+             <iframe type="application/pdf" src="<?= base_url(); ?>/File/contoh3.pdf" width="1000" height="1200"></iframe>
          </center>
      </div>
  </div>
